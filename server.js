dad22d7faf83affc3ecdcabaf129933ad28c3a38
Kiwi137 <heyuechen137@gmail.com>
2017-02-12 17:36:35 -0500

    Added projects page

--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.get('/bad', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects Page'
+	})
+});
+
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}`)
 });
